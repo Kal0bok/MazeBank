c7b31fdebae00de30cc2dbed2fab863035a4d707
Bogdan <bogdan1ptlvt@gmail.com>
2025-10-24 17:11:57 +0300

    Izveidoju metodi virknesParbaude

--- src/mazebank/Metodes.java
+++ src/mazebank/Metodes.java
@@ -1,7 +1,19 @@
 package mazebank;
 
+import java.util.regex.Pattern;
+
+import javax.swing.JOptionPane;
+
 public class Metodes {
 	
-	
+	public static String virknesParbaude(String zinojums) {
+        String virkne;
+        do {
+            virkne = JOptionPane.showInputDialog(zinojums);
+            if (virkne == null)
+                return null;
+        } while (!Pattern.matches("^[\\p{L} .]+$", virkne));
+        return virkne;
+    }
 
 }
