da2e447a0a6ae47498f4c79941be4f664fc96f60
Bogdan <bogdan1ptlvt@gmail.com>
2025-10-24 16:50:28 +0300

    Izveidoju pamatu

--- src/mazebank/bankss.java
+++ src/mazebank/bankss.java
@@ -1,7 +1,20 @@
 package mazebank;
 
-public class bankss {
+import java.util.ArrayList;
 
-	//iesakts darbs
+public class bankss {
+	
+	private static final String[] bankas = {"Swedbank", "SEB", "Citadele"};
+    private static final String[] veidi = {"NoreKarte", "KredKarte"};
+    private static final String[] atbilde = {"Jā", "Nē"};
+
+    public static void main(String[] args) {
+    	String izvele;
+        int izvelesID;
+        String[] darbibas = {"Jauns konts", "Noņemt kontu",
+                "Kontu saraksts", "Kārtot pēc atlikuma", "Bankomāts",
+                "Aizvērt programmu"};
+        ArrayList<Object> konti = new ArrayList<>();
+    }
 	
 }
