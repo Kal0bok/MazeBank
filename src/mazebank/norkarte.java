299c537ee8e582ac05fdc4e79bfc32d8aaa040d1
Bogdan <bogdan1ptlvt@gmail.com>
2025-10-24 18:48:22 +0300

    Izveidoju metodi randomGenerate

--- src/mazebank/norkarte.java
+++ src/mazebank/norkarte.java
@@ -3,9 +3,11 @@ package mazebank;
 import java.time.LocalDate;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Random;
 
 public class norkarte implements Comparable<norkarte> {
-    private String banka, vards, uzvards, personasKods, smartIdKods, paraksts, kartesNumurs, cvc, pin, kontaNumurs;
+    private String banka, vards, uzvards, personasKods, 
+    smartIdKods, paraksts, kartesNumurs, cvc, pin, kontaNumurs;
     private int derigumaGadi;
     private double atlikums = 0.0;
     private LocalDate izdosanasDatums = LocalDate.now();
@@ -17,7 +19,44 @@ public class norkarte implements Comparable<norkarte> {
         bankuKodi.put("SEB", "UNLA");
         bankuKodi.put("Citadele", "PARX");
     }
-
+    
+    public norkarte(String banka, String vards, String uzvards, String personasKods, String smartIdKods,
+            int derigumaGadi, String paraksts) {
+    	this.banka = banka;
+    	this.vards = vards;
+    	this.uzvards = uzvards;
+    	this.personasKods = personasKods;
+    	this.smartIdKods = smartIdKods;
+    	this.derigumaGadi = derigumaGadi;
+    	this.paraksts = paraksts;
+    	this.derigumaDatums = izdosanasDatums.plusYears(derigumaGadi);
+    	generateRandoms();
+    }
+    
+    private void generateRandoms() {
+        Random rand = new Random();
+        kartesNumurs = String.format("%04d %04d %04d %04d", rand.nextInt(10000), rand.nextInt(10000),
+                rand.nextInt(10000), rand.nextInt(10000));
+        cvc = String.format("%03d", rand.nextInt(1000));
+        pin = String.format("%04d", rand.nextInt(10000));
+        String bankCode = bankuKodi.getOrDefault(banka, "XXXX");
+        kontaNumurs = "LV" + String.format("%02d", rand.nextInt(100)) + bankCode +
+        		String.format("%013d", rand.nextLong(10000000000000L));
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
 	@Override
 	public int compareTo(norkarte o) {
 		return 0;
