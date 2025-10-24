ff9095523b5d51c83baaa372f56e180551a21fb5
Bogdan <bogdan1ptlvt@gmail.com>
2025-10-24 17:17:14 +0300

    Izveidoju metodi skaitlaParbaude

--- src/mazebank/Metodes.java
+++ src/mazebank/Metodes.java
@@ -15,5 +15,30 @@ public class Metodes {
         } while (!Pattern.matches("^[\\p{L} .]+$", virkne));
         return virkne;
     }
+	
+	public static Double skaitlaParbaude(String zinojums, double min, double max) {
+        String ievade;
+        Double skaitlis;
+        while (true) {
+            ievade = JOptionPane.showInputDialog(null, zinojums,
+                    "Datu ievade", JOptionPane.INFORMATION_MESSAGE);
+            if (ievade == null)
+                return -1.0;
+            try {
+                skaitlis = Double.parseDouble(ievade);
+                if (skaitlis < min || skaitlis > max) {
+                    JOptionPane.showMessageDialog(null,
+                            "Norādīts nederīgs skaitlis", "Nekorekti dati",
+                            JOptionPane.WARNING_MESSAGE);
+                    continue;
+                }
+                return skaitlis;
+            } catch (NumberFormatException e) {
+                JOptionPane.showMessageDialog(null,
+                        "Netika ievadīts pareizs skaitlis", "Nekorekti dati",
+                        JOptionPane.WARNING_MESSAGE);
+            }
+        }
+    }
 
 }
